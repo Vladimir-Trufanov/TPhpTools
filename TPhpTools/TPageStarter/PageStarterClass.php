5e73e6472aa5fe9ab692ca9ba0f3532b05c53192
Vladimir-Trufanov <tve@karelia.ru>
2019-10-11 22:36:49 +0300

    Отладка TPageStarter

--- TPhpTools/TPageStarter/PageStarterClass.php
+++ TPhpTools/TPageStarter/PageStarterClass.php
@@ -13,6 +13,10 @@
 /**
  * Класс PageStarter обеспечивает запуск сессии страницы и регистрацию 
  * пользовательской информации в базе данных SQLite3
+ * 
+ * Пользовательская информация:
+ * 
+ * название (доменное имя) сайта, название страницы сайта. 
 **/
 
 // Свойства:
@@ -28,9 +32,74 @@
 
 class PageStarter
 {
+
    private $SiteName;
    private $PageName;
-   
+   private $dtime;
+   private $ip;
+   private $agent;
+   private $uri;
+
+   // *************************************************************************
+   // *      Выбираем и формируем элементы пользовательской информации        *
+   // *************************************************************************
+   private function MakeOunInfo(&$dtime,&$ip,&$agent,&$uri)
+   {
+      $agent=$_SERVER['HTTP_USER_AGENT'];
+      $uri=$_SERVER['REQUEST_URI'];
+      //$useri = 'PHP_AUTH_USER';
+      $ip = $_SERVER['REMOTE_ADDR'];
+      //$ref = '123'; //$_SERVER['HTTP_REFERER'];
+      $dtime = date('r');
+      /*
+      if($ref == "")
+      {
+         $ref = "None";
+      }
+      if($useri == "")
+      {
+         $useri = "None";
+      }
+      */
+   }
+   // *************************************************************************
+   // *      Склеить строку пользовательской информации для лог-файла         *
+   // *************************************************************************
+   private function GlueString($PageName,$dtime,$ip,$agent,$uri,$BegSess=true)
+   {
+      // Формируем префикс строки по имени страницы. Если строка формируетя в 
+      // начале сессии ($BegSess=true), то префикс заключаем в '***', если в
+      // продолжении сессии, то префикс заключаем в '==='.
+      if ($BegSess==true) $Prefix='***'.$PageName.'***';
+      else $Prefix='==='.$PageName.'===';
+      // Подклеиваем остальную пользовательскую информацию
+      $Result=
+         $Prefix.
+         "$dtime ".
+         "IP: $ip ".
+         "Agent: $agent ".
+         "URL: $uri \r\n";
+      return $Result;
+   }
+   // *************************************************************************
+   // *         Занести строку пользовательской информации в лог-файл         *
+   // *           (лог-файл разместить в корневом каталоге страницы)          *
+   // *************************************************************************
+   private function PutString($String)
+   {
+      $fp = fopen("logis.txt","a+");
+      if (flock($fp,LOCK_EX)) 
+      { 
+         fputs($fp,$String);
+         flock($fp, LOCK_UN); 
+         fclose($fp);
+      } 
+      else 
+      {
+         echo "Не могу запереть файл! [".$PageName.']';  // Далее уйти в исключение
+      }
+   }
+
    public function __construct($iSiteName,$iPageName)
    {
       $SiteName=$iSiteName;
@@ -39,78 +108,15 @@ class PageStarter
       session_start();
       if (!isset($_SESSION[$PageName]))
       {
-         $agent=$_SERVER['HTTP_USER_AGENT'];
-         $uri=$_SERVER['REQUEST_URI'];
-         //$useri = 'PHP_AUTH_USER';
-         $ip = $_SERVER['REMOTE_ADDR'];
-         //$ref = '123'; //$_SERVER['HTTP_REFERER'];
-         $dtime = date('r');
-         $entry_line = 
-            '***'.$PageName.'***'.
-            "$dtime ".
-            "IP: $ip ".
-            "Agent: $agent ".
-            "URL: $uri \r\n";
-         /*
-         if($ref == "")
-         {
-            $ref = "None";
-         }
-         if($useri == "")
-         {
-            $useri = "None";
-         }
-         */   
-         $fp = fopen("logisi.txt", "a+");
-         if (flock($fp, LOCK_EX)) 
-         { // выполнить эксплюзивное запирание
-            fputs($fp, $entry_line);
-            flock($fp, LOCK_UN); // отпираем файл
-            fclose($fp);
-            $_SESSION[$PageName]='yes';
-         } 
-         else 
-         {
-            echo "Не могу запереть файл! [".$PageName.']';
-         }
+         $this->MakeOunInfo($this->dtime,$this->ip,$this->agent,$this->uri);
+         $entry_line=$this->GlueString($this->PageName,$this->dtime,$this->ip,$this->agent,$this->uri);
+         $this->PutString($entry_line);
+         $_SESSION[$PageName]='yes';
       }
-
-      $agent = $_SERVER['HTTP_USER_AGENT'];
-      $uri = $_SERVER['REQUEST_URI'];
-      //$useri = 'PHP_AUTH_USER';
-      $ip = $_SERVER['REMOTE_ADDR'];
-      //$ref = '123'; //$_SERVER['HTTP_REFERER'];
-      $dtime = date('r');
-      $entry_line = 
-         '==='.$PageName.'==='.
-         "$dtime ".
-         "IP: $ip ".
-         "Agent: $agent ".
-         "URL: $uri \r\n";
-      $fp = fopen("logisi.txt", "a+");
-      if (flock($fp, LOCK_EX)) 
-      { // выполнить эксплюзивное запирание
-         fputs($fp, $entry_line);
-         flock($fp, LOCK_UN); // отпираем файл
-         fclose($fp);
-      } 
-      else 
-      {
-         echo "Не могу запереть файл! [".$PageName.']';
-      }
+      $this->MakeOunInfo($this->dtime,$this->ip,$this->agent,$this->uri);
+      $entry_line=$this->GlueString($this->PageName,$this->dtime,$this->ip,$this->agent,$this->uri,false);
+      $this->PutString($entry_line);
    }
-   /*
-   //----
-   $f=fopen("stat.dat","a+");
-   flock($f,LOCK_EX);
-   $count=fread($f,100);
-   @$count++;
-   ftruncate($f,0);
-   fwrite($f,$count);
-   fflush($f);
-   flock($f,LOCK_UN);
-   fclose($f);
-   */
    public function __destruct()
    {
    }
