d8031e180fa2273b176063487ddbb2ae634083d5
Vladimir-Trufanov <tve@karelia.ru>
2020-12-26 23:59:41 +0300

    Проба для параметров через json массивов

--- www/Main.php
+++ www/Main.php
@@ -20,62 +20,85 @@
 </head>
 
 <body>
+
+   <style>
+      #liAllTests           > [data-toggle="dropdown"]:before {color: white;}
+      #liBaseMaker          > [data-toggle="dropdown"]:before {color: white;}
+      #liCtrlDir            > [data-toggle="dropdown"]:before {color: white;}
+      #liDownloadFromServer > [data-toggle="dropdown"]:before {color: white;}
+      #liFixLoadTimer       > [data-toggle="dropdown"]:before {color: yellow;}
+      #liPageStarter        > [data-toggle="dropdown"]:before {color: green;}
+      #liUploadToServer     > [data-toggle="dropdown"]:before {color: red;}
+   </style>
+
    <div class="container">
    <ul>
-   <li id=AllTests class="dropdown">
+   <li id=liAllTests class="dropdown">
       <input type="checkbox">
       <a href="#" data-toggle="dropdown">Все тесты</a>
    </li>
-   <li class="dropdown">
+   <li id="liBaseMaker" class="dropdown">
       <input type="checkbox">
       <a href="#TBaseMaker" data-toggle="dropdown">TBaseMaker</a> 
    </li>
-   <li class="dropdown">
+   <li id="liCtrlDir" class="dropdown">
       <input type="checkbox">
       <a href="#" data-toggle="dropdown">TCtrlDir</a>
    </li>
-   <li class="dropdown">
+   <li id="liDownloadFromServer" class="dropdown">
       <input type="checkbox">
-      <a href="#" data-toggle="dropdown">TDownloadFromServer
-         <!--
-         <span id="s1" 
-            class='sp'><i class="fa fa-snowflake-o" aria-hidden="true"></i>
-         </span>
-         -->
-      </a>
+      <a href="#" data-toggle="dropdown">TDownloadFromServer</a>
    </li>
-   <li class="dropdown">
+   <li id="liFixLoadTimer" class="dropdown">
       <input type="checkbox">
-      <a href="#" data-toggle="dropdown">TFixLoadTimer 
-         <!--
-         <span id="s2" 
-            class='sp'><i class="fa fa-snowflake-o" aria-hidden="true"></i>
-         </span>
-         -->
-      </a>
+      <a href="#" data-toggle="dropdown">TFixLoadTimer</a>
    </li>
-   <li class="dropdown">
+   <li id=liPageStarter class="dropdown">
       <input type="checkbox">  
-      <a href="#" data-toggle="dropdown">TPageStarter 
-         <!--
-         <span id="s3" 
-            class='sp'><i class="fa fa-snowflake-o" aria-hidden="true"></i>
-         </span>
-         -->
-      </a>
+      <a href="#" data-toggle="dropdown">TPageStarter</a>
    </li>
-   <li class="dropdown">
+   <li id=liUploadToServer class="dropdown">
       <input type="checkbox">
-      <a href="#" data-toggle="dropdown">TUploadToServer
-         <!-- 
-         <span id="s4" 
-            class='sp'><i class="fa fa-snowflake-o" aria-hidden="true"></i>
-         </span>
-         -->
-      </a>
+      <a href="#" data-toggle="dropdown">TUploadToServer</a>
    </li>
    </ul>
    </div>
+
+   <?php
+   
+   $arr = array(
+      'AllTests'           => 'notest',
+      'BaseMaker'          => 'notest',
+      'CtrlDir'            => 'notest',
+      'DownloadFromServer' => 'notest',
+      'FixLoadTimer'       => 'check',
+      'PageStarter'        => 'yes',
+      'UploadToServer'     => 'no');
+
+   echo '<br>';
+   foreach ($arr as $k => $v) echo "$k => $v<br>";
+      
+   $json=json_encode($arr);
+   if ($json)
+   {
+      echo '<br>'.$json.'<br>';
+   }
+   else 
+   {
+      echo '<br>'.'Ошибка преобразования json'.'<br>';
+   }
+   
+   $arrj=json_decode($json,true);
+   if ($arrj===NULL) {echo 'Неопределенное обратное преобразование json'.'<br>';}
+   else if ($arrj===false) {echo 'Ошибка обратного преобразования json'.'<br>';}
+   else 
+   {
+      var_dump($arrj);
+      echo '<br><br>';
+      foreach ($arrj as $k => $v) echo "$k => $v<br>";
+   }
+   ?>
+   
 </body>
 </html>
 <?php
