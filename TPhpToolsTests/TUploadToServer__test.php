912d9ec902fea7e5e451d04844aa2f98859ea4de
Труфанов Владимир Евгеньевич <tve@karelia.ru>
2021-09-03 20:12:00 +0300

    Черновик четырехэтапного теста TUploadToServer

--- TPhpToolsTests/TUploadToServer__test.php
+++ TPhpToolsTests/TUploadToServer__test.php
@@ -13,17 +13,17 @@
 // echo 'Тестируется TUploadToServer_test.php'.'<br>';
 if (!(count($_FILES)>0))
 {
-   // Отмечаем начало 1 этапа "Загрузка файла для первой группы тестов"
-   $c_UploadToServer=upl1etap;           
-   MakeTitle("TUploadToServer [".$c_UploadToServer."]",'');
-   // Готовим выполнение первой группы тестов TUploadToServer  
+   // Через кукис готовим 2 этап "Выполнение первой группы тестов TUploadToServer"
+   // после будущей перезагрузки страницы из формы  
    $c_UploadToServer=prown\MakeCookie('UploadToServer',upl2etap,tStr); 
-
-   $max = 2300000;
+   // Отмечаем начало 1 этапа "Загрузка файла для первой группы тестов"
+   MakeTitle("TUploadToServer [".upl1etap."]",'');
+   // Выполняем 1 этап "Загрузка файла для первой группы тестов"
+   $max = 3200000; // 3145728 = 3Мб;
    ?>
    <form action="" method="post" enctype="multipart/form-data" id="uploadImage">
    <p>
-   <label for="image">Выберите изображение, размером не более 2 300 000 байт:<br></label>
+   <label for="image">Выберите изображение, размером не более 3M байт:<br></label>
    <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $max; ?>">
    <input type="file" name="image" id="image">
    </p>
@@ -36,47 +36,58 @@ if (!(count($_FILES)>0))
 // На следующих этапах проводим тестирование
 else
 {
-   $c_UploadToServer=prown\MakeCookie('UploadToServer');           
-   // Готовим загрузку файла для первой группы тестов TUploadToServer  
-   MakeTitle("TUploadToServer [".$c_UploadToServer."]",'');
-   
-   
-   
-   
-   
-   $c_UploadToServer=prown\MakeCookie('UploadToServer',upl1etap,tStr);           
+   $c_UploadToServer=prown\MakeCookie('UploadToServer');  
+            
+   // Перед выполнением 2 и третьего этапов готовим через кукис 4 этап 
+   // "Выполнение второй группы тестов" после будущей перезагрузки страницы 
+   // после 3 этапа  
+   if ($c_UploadToServer==upl2etap) prown\MakeCookie('UploadToServer',upl4etap,tStr); 
+   // Перед выполнением 4 этапа перенастраиваем кукис на 1 этап 
+   if ($c_UploadToServer==upl4etap) prown\MakeCookie('UploadToServer',upl1etap,tStr); 
 
-   // Выводим доступные параметры загруженного файла
-   // print_r($_FILES);
-   echo 'Загруженный файл:   '.$_FILES['image']['name'].    '<br>';
-   echo 'Тип файла:          '.$_FILES['image']['type'].    '<br>';
-   if (isset($_FILES['image']['tmp_name']))
-   echo 'Временный файл:     '.$_FILES['image']['tmp_name'].'<br>';
-   echo 'Состояние загрузки: '.$_FILES['image']['error'].   '<br>';
-   echo 'Размер в байтах:    '.$_FILES['image']['size'].    '<br>';
-   // Настраиваем текущий каталог
-   $CurrDir=$_SERVER['DOCUMENT_ROOT'];  //'c:\TPhpTools\www';
-   chdir($CurrDir);
-   // 
-   $is=test_Construct($shellTest);
-   if ($is<>Ok) PointMessage('=== '.$is.' ===<br>');
+   // Если требуется выполнение 1 группы тестов
+   if ($c_UploadToServer==upl2etap)
+   {
+      // Отмечаем выполнение первой группы тестов TUploadToServer  
+      MakeTitle("TUploadToServer [".upl2etap."]",'');
+      // Выводим доступные параметры загруженного файла
+      // print_r($_FILES);
+      echo 'Загруженный файл:   '.$_FILES['image']['name'].    '<br>';
+      echo 'Тип файла:          '.$_FILES['image']['type'].    '<br>';
+      if (isset($_FILES['image']['tmp_name']))
+      echo 'Временный файл:     '.$_FILES['image']['tmp_name'].'<br>';
+      echo 'Состояние загрузки: '.$_FILES['image']['error'].   '<br>';
+      echo 'Размер в байтах:    '.$_FILES['image']['size'].    '<br>';
+      // Настраиваем текущий каталог
+      $CurrDir=$_SERVER['DOCUMENT_ROOT'];  //'c:\TPhpTools\www';
+      chdir($CurrDir);
+      // 
+      $is=test_Construct($shellTest);
+      if ($is<>Ok) PointMessage('=== '.$is.' ===<br>');
 
-   
-   /*
-   $max = 2300000;
-   ?>
-   <form action="" method="post" enctype="multipart/form-data" id="uploadImage">
-   <p>
-   <label for="image">Выберите изображение, размером не более 2 300 000 байт:<br></label>
-   <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $max; ?>">
-   <input type="file" name="image" id="image">
-   </p>
-   <p>
-   <input type="submit" name="upload" id="upload" value="Загрузите">
-   </p>
-   </form>
-   <?php
-   */
+      // Отмечаем начало 3 этапа "Загрузка файла для второй группы тестов"
+      MakeTitle("TUploadToServer [".upl3etap."]",'');
+      //
+      $max = 2300000;
+      ?>
+      <form action="" method="post" enctype="multipart/form-data" id="uploadImage">
+      <p>
+      <label for="image">Выберите изображение, размером не более 2 300 000 байт:<br></label>
+      <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $max; ?>">
+      <input type="file" name="image" id="image">
+      </p>
+      <p>
+      <input type="submit" name="upload" id="upload" value="Загрузите">
+      </p>
+      </form>
+      <?php
+   }
+   // Если требуется выполнение второй группы тестов
+   if ($c_UploadToServer==upl4etap)
+   {
+      // Отмечаем выполнение первой группы тестов TUploadToServer  
+      MakeTitle("TUploadToServer [".upl4etap."]",'');
+   }
 }
 // ****************************************************************************
 // *                  Создать новый каталог и задать его режим                *
