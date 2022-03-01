364823246a08f07f91c34665d0e5c0d2a674f26e
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2022-03-01 16:29:01 +0300

    Update PageStarterClass.php

--- TPhpTools/TPageStarter/PageStarterClass.php
+++ TPhpTools/TPageStarter/PageStarterClass.php
@@ -8,11 +8,12 @@
 
 //                                                   Автор:       Труфанов В.Е.
 //                                                   Дата создания:  11.10.2019
-// Copyright © 2019 tve                              Посл.изменение: 22.10.2019
+// Copyright © 2019 tve                              Посл.изменение: 01.03.2022
 
 /**
  * Класс PageStarter обеспечивает запуск сессии страницы и регистрацию 
  * пользовательской информации в базе данных SQLite3
+ * (на 01.03.2022 пока сохранение информации только в лог-файле)
  * 
  * Пользовательская информация:
  * 
@@ -24,19 +25,58 @@
 // $Page - название страницы сайта;
 // $Uagent - браузер пользователя;
 
+// Методы:
+//
+// Message - вывести сообщение в лог-файл
+
 define ("BeginSession",  1); // Флаг первого обращения к объекту в сессии  
 
 class PageStarter
 {
-
    private $Page;    // страница сайта
+   private $LogName; // имя лог-файла
    private $dtime;
    private $ip;
    private $Uagent;  // браузер пользователя
    private $uri;
 
+   public function __construct($iPage='',$iLogName='logis')
+   {
+      // Назначаем имя лог-файла
+      $this->LogName=$iLogName;
+      // Фиксируем страницу, стартовавшую сессией
+      $this->Page=$iPage;
+      // Стартуем сессию
+      session_start();
+      // Регистрируем первое открытие страницы в сессии
+      if (!isset($_SESSION[$this->Page]))
+      {
+         $this->MakeOunInfo($this->dtime,$this->ip,$this->Uagent,$this->uri);
+         $entry_line=$this->GlueString($this->Page,$this->dtime,$this->ip,$this->Uagent,$this->uri);
+         $this->PutString($entry_line);
+         $_SESSION[$this->Page]=BeginSession; // отметили первое обращение к странице в сессии
+      }
+      // Регистрируем повторные обращения к странице в сессии
+      else
+      {
+         $this->MakeOunInfo($this->dtime,$this->ip,$this->Uagent,$this->uri);
+         $entry_line=$this->GlueString($this->Page,$this->dtime,$this->ip,$this->Uagent,$this->uri,false);
+         $this->PutString($entry_line);
+      }
+   }
+   public function __destruct()
+   {
+   }
    // *************************************************************************
-   // *      Выбираем и формируем элементы пользовательской информации        *
+   // *                          Вывести сообщение в лог-файл                 *
+   // *************************************************************************
+   public function Message($messa)
+   {
+      $entry_line=$this->Page.': '.$messa;
+      $this->PutString($entry_line);
+   }
+   // *************************************************************************
+   // *      Выбрать и сформировать элементы пользовательской информации      *
    // *************************************************************************
    private function MakeOunInfo(&$dtime,&$ip,&$Uagent,&$uri)
    {
@@ -62,11 +102,16 @@ class PageStarter
    // *************************************************************************
    private function GlueString($Page,$dtime,$ip,$Uagent,$uri,$BegSess=true)
    {
+      // Не выводим префикс, когда на это указано
+      if ($Page=='') $Prefix='';
       // Формируем префикс строки по имени страницы. Если строка формируется в 
       // начале сессии ($BegSess=true), то префикс заключаем в '***', если в
       // продолжении сессии, то префикс заключаем в '==='.
-      if ($BegSess==true) $Prefix='***'.$Page.'***';
-      else $Prefix='==='.$Page.'===';
+      else
+      {
+         if ($BegSess==true) $Prefix='***'.$Page.'***';
+         else $Prefix='==='.$Page.'===';
+      }
       // Подклеиваем остальную пользовательскую информацию
       $Result=
          $Prefix.
@@ -80,9 +125,9 @@ class PageStarter
    // *         Занести строку пользовательской информации в лог-файл         *
    // *           (лог-файл разместить в корневом каталоге страницы)          *
    // *************************************************************************
-   private function PutString($String,$Page)
+   private function PutString($String)
    {
-      $fp = fopen("logis.txt","a+");
+      $fp = fopen($this->LogName.".txt","a+");
       if (flock($fp,LOCK_EX)) 
       { 
          fputs($fp,$String);
@@ -91,32 +136,8 @@ class PageStarter
       } 
       else 
       {
-         echo "Не могу запереть файл! [".$Page.']';  // Далее уйти в исключение
+         echo "Не могу запереть файл! [".$this->LogName.".txt".']';  // Далее уйти в исключение
       }
-   }
-
-   public function __construct($iPage)
-   {
-      $this->Page=$iPage;
-      session_start();
-      // Регистрируем первое открытие страницы в сессии
-      if (!isset($_SESSION[$this->Page]))
-      {
-         $this->MakeOunInfo($this->dtime,$this->ip,$this->Uagent,$this->uri);
-         $entry_line=$this->GlueString($this->Page,$this->dtime,$this->ip,$this->Uagent,$this->uri);
-         $this->PutString($entry_line,$this->Page);
-         $_SESSION[$this->Page]=BeginSession; // отметили первое обращение к странице в сессии
-      }
-      // Регистрируем повторные обращения к странице в сессии
-      else
-      {
-         $this->MakeOunInfo($this->dtime,$this->ip,$this->Uagent,$this->uri);
-         $entry_line=$this->GlueString($this->Page,$this->dtime,$this->ip,$this->Uagent,$this->uri,false);
-         $this->PutString($entry_line,$this->Page);
-      }
-   }
-   public function __destruct()
-   {
    }
 } 
 // *************************************************** PageStarterClass.php ***
